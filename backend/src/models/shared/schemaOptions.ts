1561f2f32413f50b0ac39670f3ba50f312736ff3
KelvinBeliber <kabeliber@up.edu.ph>
2026-08-09 02:18:37 +0800

    feat(backend): add shared mongoose schema base

---
+++ backend/src/models/shared/schemaOptions.ts
@@ -0,0 +1,15 @@
+import { SchemaOptions } from 'mongoose';
+
+export const schemaOptions: SchemaOptions<any> = {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: (_doc, ret: Record<string, unknown>) => {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
+      return ret;
+    },
+  },
+  toObject: { virtuals: true },
+};
